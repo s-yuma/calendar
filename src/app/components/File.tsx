802060197365263a61a9a2a8a512c0b87152457a
s-yuma <yuma.sueishi@gmail.com>
2024-01-06 22:33:28 +0900

    w

---
+++ src/app/components/File.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from "next/link"
+
+
+const File = () => {
+  return (
+    <div>
+        <Link href="/" >FILE</Link>
+    </div>
+  )
+}
+
+export default File
